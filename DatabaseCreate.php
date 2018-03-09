b283f761151daff3d812657ded27e5896decd81b
Diego Azevedo <diego.willian@hotmail.com>
2018-03-09 14:05:28 -0300

    Adapted for laravel 5.5

--- DatabaseCreate.php
+++ DatabaseCreate.php
@@ -39,26 +39,26 @@ class DatabaseCreate extends Command
      */
     public function handle()
     {
-        $database = env('DB_DATABASE', false);
+        $db = (object)config('database.connections.mysql');
 
-        if (! $database) {
+        if (! $db) {
             $this->info('Skipping creation of database as env(DB_DATABASE) is empty');
             return;
         }
 
         try {
-            $pdo = $this->getPDOConnection(env('DB_HOST'), env('DB_PORT'), env('DB_USERNAME'), env('DB_PASSWORD'));
+            $pdo = $this->getPDOConnection($db->host, $db->port, $db->username, $db->password);
 
             $pdo->exec(sprintf(
                 'CREATE DATABASE IF NOT EXISTS %s CHARACTER SET %s COLLATE %s;',
-                $database,
-                env('DB_CHARSET'),
-                env('DB_COLLATION')
+                $db->database,
+                $db->charset,
+                $db->collation
             ));
 
-            $this->info(sprintf('Successfully created %s database', $database));
+            $this->info(sprintf('Successfully created %s database', $db->database));
         } catch (PDOException $exception) {
-            $this->error(sprintf('Failed to create %s database, %s', $database, $exception->getMessage()));
+            $this->error(sprintf('Failed to create %s database, %s', $db->database, $exception->getMessage()));
         }
     }
 
